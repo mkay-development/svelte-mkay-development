bb4abbb11eb3db51ff523d124ec4e65a1b71a5f8
Martz <github@jmartz.de>
2023-02-07 15:39:18 +0100

    update title homepage, start with content homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,61 @@
-<div class="grid grid-cols-6">
-  <div class="col-span-6 md:col-span-3">Test</div>
+<svelte:head>
+  <title>Startseite - MkaY Development</title>
+  <meta
+    name="description"
+    content="Wir entwickeln und betreiben Webseiten und spezielle Software für ihr Unternehmen. Außerdem helfen wir ihnen bei der Kommunikation mit ihren Kunden per Email. Dazu setzen wir auf moderne Technologien wie Laravel, VueJs und Svelte.
+  Melde dich jetzt für ein unverbindliches Angebot."
+  />
+</svelte:head>
+
+<div class="grid grid-cols-6 gap-3">
   <div class="col-span-6 md:col-span-3">
-    Hallo Welt
+    <img
+      src="https://avatars.githubusercontent.com/u/20771653?v=4"
+      alt=""
+      class="w-32 rounded-full mx-auto"
+    />
+    <h2 class="block text-center mt-3">Jonathan Martz</h2>
+    <span class="block text-center">Geschäftsführer & Entwickler</span>
+    <p class="text-sm block text-center">
+      Herzlich Willkommen bei der MkaY Development UG. Hier findet ihr alle
+      Informationen zu meiner Firma. Wir entwickeln kleine Webseite und Services
+      für Unternehmen im Oberbergischen Kreis.
+    </p>
+  </div>
+  <div class="col-span-6 md:col-span-3">
+    <div class="grid grid-cols-6 gap-3">
+      <div class="col-span-6">
+        <section class="card bg-gray-300 px-2 py-2">
+          <img
+            src="https://avatars.githubusercontent.com/u/20771653?v=4"
+            alt=""
+            class="rounded-full w-16 mx-auto"
+          />
+          <h2 class="block text-center font-bold mt-2">Kunde Xyz</h2>
+          <p class="text-sm block text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit.
+            ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </section>
+      </div>
+      <div class="col-span-6">
+        <section class="card bg-gray-300 px-2 py-2">
+          <img
+            src="https://avatars.githubusercontent.com/u/20771653?v=4"
+            alt=""
+            class="rounded-full w-16 mx-auto"
+          />
+          <h2 class="block text-center font-bold mt-2">Kunde Xyz</h2>
+          <p class="text-sm block text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
+            ipsum dolor sit amet consectetur, adipisicing elit.
+            ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </section>
+      </div>
+    </div>
   </div>
 </div>
