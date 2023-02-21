111ede1e0955201143c0e843733243cc28ce5fb3
Martz <github@jmartz.de>
2023-02-21 22:38:26 +0100

    update content homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   <title>Startseite - MkaY Development</title>
   <meta
     name="description"
-    content="Wir entwickeln und betreiben Webseiten und spezielle Software für ihr Unternehmen. Außerdem helfen wir ihnen bei der Kommunikation mit ihren Kunden per Email."
+    content="Wir entwickeln und betreiben Webseiten und spezielle Software für ihr Unternehmen. Außerdem helfen wir Ihnen bei der Kommunikation mit ihren Kunden per Email."
   />
 </svelte:head>
 
@@ -11,7 +11,7 @@
     <img
       src="https://avatars.githubusercontent.com/u/20771653?v=4"
       alt=""
-      class="w-32 rounded-full mx-auto"
+      class="w-24 rounded-full mx-auto"
     />
     <h2 class="block text-center mt-3">Jonathan Martz</h2>
     <span class="block text-center">Geschäftsführer & Entwickler</span>
@@ -22,39 +22,24 @@
     </p>
   </div>
   <div class="col-span-6 md:col-span-3">
-    <div class="grid grid-cols-6 gap-3">
-      <div class="col-span-6">
-        <section class="card bg-gray-300 px-2 py-2">
-          <img
-            src="https://avatars.githubusercontent.com/u/20771653?v=4"
-            alt=""
-            class="rounded-full w-16 mx-auto"
-          />
-          <h2 class="block text-center font-bold mt-2">Kunde Xyz</h2>
-          <p class="text-sm block text-center">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
-            ipsum dolor sit amet consectetur, adipisicing elit.
-            ipsum dolor sit amet consectetur, adipisicing elit.
-          </p>
-        </section>
-      </div>
-      <div class="col-span-6">
-        <section class="card bg-gray-300 px-2 py-2">
-          <img
-            src="https://avatars.githubusercontent.com/u/20771653?v=4"
-            alt=""
-            class="rounded-full w-16 mx-auto"
-          />
-          <h2 class="block text-center font-bold mt-2">Kunde Xyz</h2>
-          <p class="text-sm block text-center">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem
-            ipsum dolor sit amet consectetur, adipisicing elit.
-            ipsum dolor sit amet consectetur, adipisicing elit.
-          </p>
-        </section>
-      </div>
-    </div>
+    <h2 class="font-bold block text-center text-lg">Verantwortlicher:</h2>
+    <p class="block text-center text-sm">
+      Jonathan Martz <br />
+    </p>
+    <h2 class="font-bold block text-center text-lg mt-3">Adresse:</h2>
+    <p class="block text-center text-sm">
+      Othestrasse 2 <br />
+      51702 Bergneustadt <br />
+      Germany <br />
+    </p>
+    <h2 class="font-bold block text-center text-lg mt-3">Kontakt:</h2>
+    <p class="block text-center text-sm">
+      Tel.: <a href="tel:+49 174 5141698" class="text-blue-600">0174 5141698</a>
+      <br />
+      Mail:
+      <a href="mailto:kontakt@mkay.dev" class="text-blue-600"
+        >kontakt@mkay.dev</a
+      >
+    </p>
   </div>
 </div>
