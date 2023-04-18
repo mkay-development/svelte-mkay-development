d590acca78f70f08bcaf353982d9a7cf6b1ad11c
Jonathan <github@jmartz.de>
2023-04-18 17:26:43 +0200

    start with adding shopware

---
+++ src/lib/components/ProductTableView.svelte
@@ -0,0 +1,33 @@
+<script>
+    import {
+        getCategory,
+        getCategoryProducts,
+    } from "@shopware-pwa/shopware-6-client";
+    import { onMount } from "svelte";
+
+    export let identifier;
+
+    let products = [];
+    let categorie = {};
+
+    onMount(async () => {
+        categorie = await getCategory(identifier);
+        let tmp = await getCategoryProducts(identifier);
+        products = tmp.elements;
+    });
+</script>
+
+{#if categorie.name}
+    <h3 class="font-bold mt-6">
+        {categorie.name}
+    </h3>
+{/if}
+{#each products as p}
+    <div class="flex justify-between mt-3">
+        <div class="name">
+            {p.name} <br />
+            <p class="text-sm">{p.description}</p>
+        </div>
+        <div class="price">{p.calculatedPrice.unitPrice.toFixed(2)} €</div>
+    </div>
+{/each}
---
+++ src/routes/services/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import ProductTableView from "$lib/components/ProductTableView.svelte";
+    let categories = [
+        "aa208bfab4a24771a97fdcb459c4ffb2",
+        "1274c1a781934a36a80766b9101055e0",
+        "f89966ed05d74d90a8374323ad4171fa",
+    ];
+</script>
+
+{#each categories as c}
+    <ProductTableView identifier={c} />
+{/each}
